81aafbd4860785ee565508960b5bf8a36b749b76
SadiqUltra <sadikultra@gmail.com>
2015-01-02 12:27:04 +0600

    Create config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,22 @@
+<?php
+	define('SITE_NAME', 'Roll Visitor');
+	//Database host name
+	define('DB_HOST', 'localhost');
+	//Database name
+	define('DB_NAME', 'Database_Name');
+	//Database user name
+	define('DB_USER', 'root');
+	//Database password
+	define('DB_PASSWORD', '');
+	//Database table name
+	define('DB_TABLE', 'rollvisitor_log');
+	define('DB_CHARSET', '');
+	//IP api user
+	define('IP_API_USER', '');
+	//IP api key
+	define('IP_API_KEY', '');
+	//Url of rollvisitor folder
+	define('ROLLVISITIR_URL', 'http://localhost/rollvisitor/');
+	//Country flag to show
+	define('NUM_COUNTRY_SHOW', 25);
+?>
