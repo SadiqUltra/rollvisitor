78443b8f31f38c0ec00d1067ff714d397c00709c
SadiqUltra <sadikultra@gmail.com>
2015-01-02 12:27:42 +0600

    Create example-api-application.php

---
+++ example-api-application.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*************************************************************
+ * Roll Visitor
+ * @Author name: A. S. M. Sadiqul Islam & Khalid Ibn Zinnah Apu
+ * @Creation Date: July 2014
+ * @Website Url: http://rollvisitor.com
+ * @Version: 1.0.0
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @copyright (C) 2014 - RollVisitor System Inc.
+ * 
+ */
+
+	require_once 'api.php';
+
+	rollvisitor_api('FLAG', 'default');
+
+?>
